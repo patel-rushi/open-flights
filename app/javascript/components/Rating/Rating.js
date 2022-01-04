81efefb5ff217fd984bf224ff2dbc67b51bd22a6
Rushi Patel <mail.rushipatel@gmail.com>
2022-01-04 16:39:23 -0500

    Resolving stars issue exp-5

--- app/javascript/components/Rating/Rating.js
+++ app/javascript/components/Rating/Rating.js
@@ -4,8 +4,8 @@ import React from 'react'
 const Rating = (props) => {
     const score = (props.score/5)*100
     return (
-        <span class='star-wrapper'>
-            <span class='stars' style={{width: score+"%"}}></span>
+        <span className='star-wrapper'>
+            <span className='stars' style={{width: score+"%"}}></span>
         </span>
     )
 }
